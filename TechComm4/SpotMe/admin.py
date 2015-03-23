860c15fcf85a9e17c080511e880c2130835d88a7
Lydia Mankins <lydia@tilberi.res.cmu.edu>
2015-03-23 09:04:58 -0400

    Add basic index and detail views
    add basic index and detail views and accompanying templates
    add urls

--- TechComm4/SpotMe/admin.py
+++ TechComm4/SpotMe/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from models import Area
+
 # Register your models here.
+admin.site.register(Area)
--- TechComm4/SpotMe/views.py
+++ TechComm4/SpotMe/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from SpotMe.models import Area
+
+# full area view
+def index(request):
+    area_list = Area.objects.order_by('-floor')
+    context = {'area_list': area_list }
+    return render(request, 'SpotMe/index.html', context)
+
+# detail page on single area
+def detail(request, area_id):
+    area = get_object_or_404(Area, pk=area_id)
+    return render(request, 'SpotMe/detail.html', {'area': area})
