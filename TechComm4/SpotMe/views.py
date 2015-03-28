c9ccfdde9b6cd5ddec68fff81e84db0d463c1c84
Zachary Halle <zach@Zacharys-MacBook-Pro.local>
2015-03-28 12:33:09 -0400

    progress on filter page

--- TechComm4/SpotMe/views.py
+++ TechComm4/SpotMe/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404
 
 from SpotMe.models import Area
+from SpotMe.search import AreaForm
 
 # full area view
 def index(request):
@@ -12,3 +13,15 @@ def index(request):
 def detail(request, area_id):
     area = get_object_or_404(Area, pk=area_id)
     return render(request, 'SpotMe/detail.html', {'area': area})
+
+# search based on attributes
+def search(request):
+    if request.method == 'POST':
+        form = AreaForm(request.POST)
+        if form.is_valid():
+            # do filtering logic here
+
+    else:
+        form = AreaForm()
+
+    return render(request, 'SpotMe/search.html', {'form' : form})
